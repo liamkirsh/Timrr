a796c67f697a011e6390c4f3199d665bf4d29a5a
Michael Wirth <mike.wirth@gmail.com>
2016-09-18 02:18:06 +0200

    Finish last 2 charts

--- webfrontend/src/app/main/main.controller.js
+++ webfrontend/src/app/main/main.controller.js
@@ -13,6 +13,19 @@ angular.module('inspinia')
         vm.blocks = data.data;
     });
 
+    vm.formatDateLabel = function(date) {
+        var weekday = new Array(7);
+        weekday[0]=  "Su";
+        weekday[1] = "Mo";
+        weekday[2] = "Tu";
+        weekday[3] = "We";
+        weekday[4] = "Th";
+        weekday[5] = "Fr";
+        weekday[6] = "Sa";
+
+        return weekday[date.getDay()];
+    }
+
     /*
     vm.blocks = [{
 	    	from: '2016-09-17T00:00:00.000Z',
